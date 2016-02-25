5d16cb34454c2b8c84c66753bf1ba2960755b496
Askandar Kalandarov <ascandar@gmail.com>
2016-02-25 03:22:36 +0300

    HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -6,8 +6,9 @@ import ru.javawebinar.topjava.model.UserMealWithExceed;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * GKislin
@@ -16,21 +17,53 @@ import java.util.List;
 public class UserMealsUtil {
     public static void main(String[] args) {
         List<UserMeal> mealList = Arrays.asList(
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 1000),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,10,0), "Завтрак", 1000),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
         );
-        getFilteredMealsWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+        getFilteredMealsWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
 //        .toLocalDate();
 //        .toLocalTime();
     }
 
-    public static List<UserMealWithExceed>  getFilteredMealsWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+    public static List<UserMealWithExceed> getFilteredMealsWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+
+
         // TODO return filtered list with correctly exceeded field
-        System.out.println("dsds");
-        return null;
+
+        Map<Integer, Long> map = new HashMap<>();
+
+        mealList.stream()
+                .collect(Collectors.groupingBy(foo -> foo.getDateTime().getDayOfYear() + foo.getDateTime().getDayOfYear(), Collectors.summarizingInt(UserMeal::getCalories)))
+                .forEach((date,sum) -> map.put(date,sum.getSum()));
+
+        List<UserMealWithExceed> userMealWithExceeds = mealList
+                .stream()
+                .filter(s -> s.getDateTime().getHour() > startTime.getHour() && s.getDateTime().getHour() < endTime.getHour())
+                .filter(m -> m.getCalories() < caloriesPerDay)
+                .map(userMeal ->
+                        new UserMealWithExceed(
+                                userMeal.getDateTime(),
+                                userMeal.getDescription(),
+                                userMeal.getCalories(),
+                                isExceeded(map.get(userMeal.getDateTime().getDayOfYear()+userMeal.getDateTime().getDayOfYear()),caloriesPerDay)))
+                .collect(Collectors.toList());
+
+
+ /*       for (UserMealWithExceed userMealWithExceed : userMealWithExceeds) {
+            System.out.println("Time: " + userMealWithExceed.getDateTime() +
+                    "\tDescription: " + userMealWithExceed.getDescription() +
+                    "\tCalories: " + userMealWithExceed.getDescription() +
+                    "\tExceeded: "  + userMealWithExceed.isExceed());
+        }*/
+
+        return userMealWithExceeds;
+    }
+
+    private static boolean isExceeded(long day, int caloriesPerDay){
+        return day > caloriesPerDay;
     }
 }
