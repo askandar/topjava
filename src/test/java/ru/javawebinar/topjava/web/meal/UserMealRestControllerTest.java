31bcdb4e15be03414ad58f3381a88a0b91784550
Askandar Kalandarov <ascandar@gmail.com>
2016-05-15 02:58:04 +0300

    HW9_commit_1

--- src/test/java/ru/javawebinar/topjava/web/meal/UserMealRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/meal/UserMealRestControllerTest.java
@@ -15,6 +15,7 @@ import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
